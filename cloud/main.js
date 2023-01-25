2c9489fd2ab0607d4dea7a36e82d6c293a858795
emersonbarina <emersonbarina@gmail.com>
2023-01-25 20:33:15 -0300

    passing parameters of the request

--- cloud/main.js
+++ cloud/main.js
@@ -8,9 +8,9 @@ Parse.Cloud.define("hello", (request) => {
 // Create fuction
 Parse.Cloud.define("create-product", async (request) => {
 	const product = new Product();
-	product.set("name", "Produto Cloud");
-	product.set("price", 100);
-	product.set("stock", 20);
+	product.set("name", request.params.name);
+	product.set("price", request.params.price);
+	product.set("stock", request.params.stock);
 	product.set("isSelling", true);
 	const savedProduct = await product.save(null, {useMasterkey: true});
 	return savedProduct;
